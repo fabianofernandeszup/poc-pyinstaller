0ff162c758176e7d8f9a5bcd8f0dc1a67f38ceae
Fabiano Chiaretto Fernandes <58859234+fabianofernandeszup@users.noreply.github.com>
2021-11-30 10:10:50 -0300

    Update poc.py

--- poc.py
+++ poc.py
@@ -9,6 +9,12 @@ req = requests.get(url)
 req = req.text
 print("\033[33mContent:\033[0m", req)
 
+print("\n\033[1m3 - Read Github PAGES file\033[0m")
+url = "https://Orangestack-com.github.io"
+req = requests.get(url)
+req = req.text
+print("\033[33mContent:\033[0m\n", html2text.html2text(req))
+
 print("\033[1m2 - Download Github RELEASE (Zip file)\033[0m")
 url2="https://github.com/GuillaumeFalourd/git-commit-push/zipball/main"
 r = requests.get(url2)
@@ -19,11 +25,5 @@ if r.status_code == 200:
         print("\033[33mDownloaded file:\033[0m output.zip")
 else:
     print(r.text)
-
-print("\n\033[1m3 - Read Github PAGES file\033[0m")
-url = "https://Orangestack-com.github.io"
-req = requests.get(url)
-req = req.text
-print("\033[33mContent:\033[0m\n", html2text.html2text(req))
 
 print("\n\033[31mEND of execution\033[0m")
